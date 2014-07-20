709d5142d75b8d18421a194bbbf509e122417cd0
Erik August Johnson <erik@erikaugust.com>
2014-07-20 10:28:05 -0400

    Update base_include.php
    Moves the .inis to ./config from ./settings

--- php/base_include.php
+++ php/base_include.php
@@ -4,8 +4,8 @@ date_default_timezone_set('UTC');
 require_once __DIR__.'/vendor/autoload.php';
 
 // CONSTANTS
-define("DB_SETTINGS_DIR", __DIR__.'/settings/database.ini');
-//define("MEMCACHE_SETTINGS_DIR", __DIR__.'/settings/memcache.ini');
+define("DB_SETTINGS_DIR", __DIR__.'/config/database.ini');
+//define("MEMCACHE_SETTINGS_DIR", __DIR__.'/config/memcache.ini');
 
 define("MEMCACHE_LIBRARY", 'memcache');
 define("MEMCACHE_HOST", '127.0.0.1');
